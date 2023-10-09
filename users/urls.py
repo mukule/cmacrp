7e2e6706f3e56809ef2b31255ebc6502fb002342
mukule <nelsonmasibo6@gmail.com>
2023-10-09 12:27:22 +0300

    users admin to django admin remove hrs

--- users/urls.py
+++ users/urls.py
@@ -15,6 +15,5 @@ urlpatterns = [
     path("password_change", views.password_change, name="password_change"),
     path("password_reset", views.password_reset_request, name="password_reset"),
     path('reset/<uidb64>/<token>', views.passwordResetConfirm, name='password_reset_confirm'),
-    path("hrs/", views.hrs, name="hrs"),
     path("not_authorized/", views.not_authorized, name="not_authorized"),
 ]
